71cc7705dacd9efac0e1787449e35fe007065c58
ekan <hmcx527@gmail.com>
2025-12-09 17:11:12 +0800

    截断问题

--- QRcode.js
+++ QRcode.js
@@ -1,35 +1,31 @@
-import { close_api, delay, send, startService } from "./utils/utils.js";
+import {close_api, delay, send, startService} from "./utils/utils.js";
+import fs from 'fs';
 
 async function qrcode() {
+    let api = startService()
+    await delay(2000)
 
-  // 启动服务
-  let api = startService()
-  await delay(2000)
+    try {
+        let result = await send(`/login/qr/key`, "GET", {})
+        if (result.status === 1) {
+            // 只输出 key
+            console.log("key:", result.data.qrcode)
 
-  try {
-    // 登录请求
-    let result = await send(`/login/qr/key`, "GET", {})
-    if (result.status === 1) {
-      console.log("请求成功！")
-      console.log("第一行是key,第二行是二维码链接")
-      console.log(result.data.qrcode)
-      console.log(result.data.qrcode_img)
-      console.log("----------")
-      console.log(result)
-    } else {
-      console.log("响应内容")
-      console.dir(result, { depth: null })
-      throw new Error("请求失败！请检查")
+            // 只保存图片
+            if (result.data.qrcode_img) {
+                const base64Data = result.data.qrcode_img.replace(/^data:image\/\w+;base64,/, '')
+                fs.writeFileSync('qrcode.png', Buffer.from(base64Data, 'base64'))
+            }
+        } else {
+            throw new Error("请求失败")
+        }
+    } finally {
+        close_api(api)
     }
-  } finally {
-    close_api(api)
-  }
 
-  if (api.killed) {
-    // 强制关闭进程
-    // 必须强制关闭，不然action不会停止
-    process.exit(0)
-  }
+    if (api.killed) {
+        process.exit(0)
+    }
 }
 
 qrcode()
